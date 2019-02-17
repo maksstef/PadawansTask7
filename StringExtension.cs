3f598033011c00a48b640f1d1d41cb211338442e
mxmstfnk <boss.canta@mail.ru>
2019-02-17 13:37:08 +0300

    1

---
+++ StringExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PadawansTask7
+{
+    public class StringExtension
+    {
+        public static void OrderStringsByLength(string[] array)
+        {
+            // put your code here
+            string sg = "";
+            for (int i = 0; i < array.Length; ++i)
+            {
+                for (int j = 0; j < array.Length; ++j)
+                {
+                    if (array[i].Length < array[j].Length)
+                    {
+                        sg = array[i];
+                        array[i] = array[j];
+                        array[j] = sg;
+                    }
+                }
+            }
+
+        }
+    }
+}
